8d37dc27a6321a89dc477618fa00fba8fe9c3ae3
Alex Turner <turneral1@grinnell.edu>
2024-04-01 21:10:08 -0700

    Started playing around with different colors. Added cute gif to title bar

--- quartz/bin/process_file.sh
+++ quartz/bin/process_file.sh
@@ -1,16 +1,17 @@
-#!/usr/bin/fish
+#!/bin/bash
 
-set file $argv[0]
+set file $argv[1]
 # Footnote invocation replacement (from LW format)
 # Matches eg [[16]](fdsav)        ->  [^16]
-sed -i '' -E 's/\[\[([^]]*)\]\].*\)/\[\^\1\]/g' $file
+sed -E 's/\[\[([^\]]*)\]\].*\)/\[\^\1\]/g' $file >$file.tmp && mv $file.tmp $file
+echo "1"
 
 # Include trailing punctuation in MD links so that favicons look good
 #                 [link text](url). -> [link text.](url)
 # sed -i '' -E 's/\[(?<linkText>.*)\]\((?<url>.*)\)(?<punctuation>[\"\`\)\]\}\.\,\?\!]+)/\[\k<linkText>\k<punctuation>\]\(\k<url>\)/g' $file
-perl -pe 's/([\(\”\“\"\[]*)\[([^\]]+)\]\([^#](.*?)\)([\”\"\`\)\”\]\}\.\,\?:\!”]+)\s/\[$1$2$4\]\($3\)/gm;t' $file
+perl -pe 's/([\(\”\“\"\[]*)\[([^\]]+)\]\([^#](.*?)\)([\”\"\`\)\”\]\}\.\,\?:\!”]+)\s/\[$1$2$4\]\($3\)/gm;t' $file >$file.tmp && mv $file.tmp $file
 
 # Footnote content replacement (from LW format)
 #               16. **(garbage)** -> [^16]:
-sed -i '' -E 's/([0-9]+)\. \*\*.*\)\*\*/\[\^\1\]: /g' $file
+perl -pe 's/([0-9]+)\. \*\*.*\)\*\*/\[\^\1\]: /g' $file >$file.tmp && mv $file.tmp $file
 # TODO there are two extra newlines still, but apparently obsidian doesn't care.
--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -8,6 +8,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const baseDir = pathToRoot(fileData.slug!)
   return (
     <h1 class={classNames(displayClass, "page-title")}>
+    <img src={"static/pond.gif"} class="header-img"></img>
       <a href={baseDir}>{title}</a>
     </h1>
   )
