f76264a1597b54ab9b47ae5d288c8b1cb19be8e8
Alex Turner <turneral1@grinnell.edu>
2024-04-15 09:11:05 -0700

    --amend

--- quartz/bin/download_images.fish
+++ quartz/bin/download_images.fish
@@ -14,15 +14,19 @@ end
 
 # --- Main Logic ---
 
-set markdown_files (string split "\n" (find . -name "*.md"))
+# set -l default_markdown_files (string split "\n" (find . -name "*.md"))
+# set -l markdown_files $argv[1] or set -l markdown_files $default_markdown_files
+set -l markdown_files $argv[1]
+echo "Markdown files: $markdown_files"
 
 # 1. Find all image URLs in Markdown files
-set image_urls (grep -oE 'http.*\.(jpg|png)$' $markdown_files)
+set -l image_urls (grep -oE 'http.*\.(jpg|png)' $markdown_files)
+echo "Image URLs: $image_urls"
 
 # 2. Download each image
 for url in $image_urls
     download_image $url
-    sed -i "s|$url|/tmp/images/$(basename $url)|g" $markdown_files
+    sed -i '' "s|$url|/tmp/images/$(basename $url)|g" $markdown_files
 end
 
 echo "Image download and replacement complete!"
