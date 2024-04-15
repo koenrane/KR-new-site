badadbf856a64d808b298013dddbde786e5bfc08
Alex Turner <turneral1@grinnell.edu>
2024-04-15 15:51:25 -0700

    Unsuccessful attempt at fixing favicon hosting

--- quartz/plugins/transformers/linkfavicons.ts
+++ quartz/plugins/transformers/linkfavicons.ts
@@ -2,35 +2,55 @@ import { visit } from "unist-util-visit"
 import { QuartzTransformerPlugin } from "../types"
 import fs from "fs"
 import https from "https"
+import path from "path"
 
-const MAIL_PATH = "/static/images/mail.svg"
+const MAIL_PATH = "~/Documents/quartz/quartz/static/images/mail.svg"
 
-const MaybeSaveFavicon = (node: any, hostname: string) => {
-  // Save the favicon to the local storage and return path
-  const sanitizedHostname = hostname.replace(/\./g, "_")
-  const localPath = `/static/images/external-favicons/${sanitizedHostname}.png`
+function downloadFileSync(url, destPath) {
+  const file = fs.createWriteStream(destPath)
+  const response = https.get(url)
+  response.pipe(file)
 
-  if (!fs.existsSync(localPath)) {
-    const googleFaviconURL = `https://www.google.com/s2/favicons?sz=64&domain=${hostname}`
-    const file = fs.createWriteStream(localPath)
+  return new Promise((resolve, reject) => {
+    file.on("finish", () => {
+      file.close()
+      resolve()
+    })
+    response.on("error", (err) => {
+      fs.unlinkSync(destPath)
+      reject(err)
+    })
+    file.on("error", (err) => {
+      fs.unlinkSync(destPath)
+      reject(err)
+    })
+  })
+}
 
-    https
-      .get(googleFaviconURL, (res) => {
-        res.pipe(file)
+const QUARTZ_FOLDER = "~/Documents/quartz/quartz"
+const FAVICON_FOLDER = "/static/images/external-favicons"
 
-        file.on("finish", () => {
-          file.close()
-          console.log("Image downloaded successfully!")
-        })
-      })
-      .on("error", (err) => {
-        fs.unlink(localPath, () => {}) // Delete the file if it was partially downloaded
-        console.error("Error downloading image:", err)
-        return ""
-      })
-  }
+async function MaybeSaveFavicon(hostname: string) {
+  return new Promise((resolve, reject) => {
+    // Save the favicon to the local storage and return path
+    const sanitizedHostname = hostname.replace(/\./g, "_")
+    const localPath = `${QUARTZ_FOLDER}/${sanitizedHostname}.png`
+    const quartzPath = `${FAVICON_FOLDER}/${sanitizedHostname}.png`
 
-  return localPath
+    if (!fs.existsSync(localPath)) {
+      const googleFaviconURL = `https://www.google.com/s2/favicons?sz=64&domain=${hostname}`
+      try {
+        downloadFileSync(googleFaviconURL, localPath)
+        resolve(quartzPath)
+      } catch (error) {
+        console.log(error)
+        console.error(`Failed to download favicon for ${hostname}`)
+        reject(null)
+      }
+    } else {
+      resolve(quartzPath)
+    }
+  })
 }
 
 const CreateFaviconElement = (urlString: string, description: string = "") => {
@@ -64,7 +84,7 @@ export const AddFavicons: QuartzTransformerPlugin = () => {
               if (node.tagName === "a") {
                 const linkNode = node
                 // Remove the "external-icon" elements, hidden anyways
-                if (linkNode?.children) {
+                if (linkNode?.children && linkNode.children.length > 0) {
                   linkNode.children = linkNode.children.filter(
                     (child) => child.properties?.class !== "external-icon",
                   )
@@ -78,33 +98,39 @@ export const AddFavicons: QuartzTransformerPlugin = () => {
                     img = CreateFaviconElement(MAIL_PATH, "email address")
                   } else {
                     const url = new URL(linkNode.properties.href)
-                    const imgPath = MaybeSaveFavicon(linkNode, url.hostname)
-                    if (!imgPath) return // Couldn't load/save favicon
-                    img = CreateFaviconElement(imgPath, url.hostname)
-                  }
-                  var toPush = img
+                    MaybeSaveFavicon(url.hostname)
+                      .then(function (imgPath) {
+                        console.log(imgPath)
+                        img = CreateFaviconElement(imgPath, url.hostname)
+                        var toPush = img
 
-                  const lastChild = linkNode.children[linkNode.children.length - 1]
-                  if (lastChild && lastChild.type === "text" && lastChild.value) {
-                    const textContent = lastChild.value
-                    const charsToRead = Math.min(4, textContent.length)
-                    const lastFourChars = textContent.slice(-1 * charsToRead)
-                    lastChild.value = textContent.slice(0, -4)
+                        const lastChild = linkNode.children[linkNode.children.length - 1]
+                        if (lastChild && lastChild.type === "text" && lastChild.value) {
+                          const textContent = lastChild.value
+                          const charsToRead = Math.min(4, textContent.length)
+                          const lastFourChars = textContent.slice(-1 * charsToRead)
+                          lastChild.value = textContent.slice(0, -4)
 
-                    // Create a new span
-                    toPush = {
-                      type: "element",
-                      tagName: "span",
-                      children: [
-                        { type: "text", value: lastFourChars },
-                        img, // Append the previously created image
-                      ],
-                      properties: {
-                        style: "white-space: nowrap;",
-                      },
-                    }
+                          // Create a new span
+                          toPush = {
+                            type: "element",
+                            tagName: "span",
+                            children: [
+                              { type: "text", value: lastFourChars },
+                              img, // Append the previously created image
+                            ],
+                            properties: {
+                              style: "white-space: nowrap;",
+                            },
+                          }
+                        }
+                        linkNode.children.push(toPush)
+                        console.log(toPush)
+                      })
+                      .catch((error) => {
+                        console.error("Error downloading favicon:", error)
+                      })
                   }
-                  linkNode.children.push(toPush)
                 }
               }
             })
