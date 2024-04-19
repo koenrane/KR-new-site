52a1e306e381609acfb46311d2eee9b9ab442995
Alex Turner <turneral1@grinnell.edu>
2024-04-19 12:03:27 -0700

    --amend

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -4,21 +4,20 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
-      <div class="container" style="position:relative;">
-        <div class="container" style="position:absolute;top:0.6em;left:12%;">
-          <h1 style="margin-bottom:0px;line-height:.1em;text-align:right">404</h1>
-          <p style="font-size:1em;text-align:right">
-            {" "}
+      <div style="position:relative;">
+        <div style="left:5%;">
+          <h1 class="header-404">404</h1>
+          <p style="max-width:30%;text-align-last:justify;font-size:1em;">
             That page doesn't exist. <br />
-            But don't leave! <br />
-            There are other fish in the pond.
+            But don't leave! There <br />
+            are other fish in the pond.
           </p>
         </div>
 
         <img
           src="/static/images/turntrout-art-transparent.png"
           class="no-select"
-          style="max-width:70%;position:absolute;right:-10px"
+          style="max-width:70%;left:13%;top:-50%;position:absolute"
         ></img>
       </div>
     </article>
