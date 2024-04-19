6008a48b7d99fb894975a6084b691f2094d40844
Alex Turner <turneral1@grinnell.edu>
2024-04-19 11:00:11 -0700

    Improved 404 page and responsive text features

--- quartz/components/pages/404.tsx
+++ quartz/components/pages/404.tsx
@@ -5,16 +5,20 @@ const NotFound: QuartzComponent = ({ cfg }: QuartzComponentProps) => {
   return (
     <article class="popover-hint">
       <div class="container" style="position:relative;">
-        <h1 style="position:absolute;top:-.5em;left:4.3em">404</h1>
-        <p style="top:2em;left:2em;" class="resize-text">
-          That page doesn't exist. But don't leave! There are other fish in the pond. 🐟
-          <a href="/posts">Go back home</a>
-        </p>
+        <div class="container" style="position:absolute;top:0.6em;left:12%;">
+          <h1 style="margin-bottom:0px;line-height:.1em;text-align:right">404</h1>
+          <p style="font-size:1em;text-align:right">
+            {" "}
+            That page doesn't exist. <br />
+            But don't leave! <br />
+            There are other fish in the pond.
+          </p>
+        </div>
 
         <img
           src="/static/images/turntrout-art-transparent.png"
           class="no-select"
-          style="max-width:70%;position:absolute;right:0px"
+          style="max-width:70%;position:absolute;right:-10px"
         ></img>
       </div>
     </article>
