9f1447b2a7416e4718ec2f4e18a65fcff63dd7f6
Alex Turner <turneral1@grinnell.edu>
2024-04-20 13:56:03 -0700

    Testing out new workflow

--- quartz/components/Authors.tsx
+++ quartz/components/Authors.tsx
@@ -1,13 +1,13 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 
-const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
+const Authors: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   if (fileData.frontmatter?.hide_metadata) {
     return null
   }
   let authors = "Alex Turner"
   if (fileData.frontmatter?.authors) {
-    authors = fileData.frontmatter?.authors
+    authors = fileData.frontmatter?.authors as string
   }
   authors = "By " + authors
   if (fileData.frontmatter?.hide_authors) {
@@ -17,10 +17,10 @@ const ArticleTitle: QuartzComponent = ({ fileData, displayClass }: QuartzCompone
   return <p class="authors content-meta">{authors}</p>
 }
 
-ArticleTitle.css = `
+Authors.css = `
 .article-title {
   margin: 2rem 0 0 0;
 }
 `
 
-export default (() => ArticleTitle) satisfies QuartzComponentConstructor
+export default (() => Authors) satisfies QuartzComponentConstructor
