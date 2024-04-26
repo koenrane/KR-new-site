1c943071e889687dd9597d9713333922c07bb2e7
Alex Turner <turneral1@grinnell.edu>
2024-04-26 13:02:28 -0700

    Fixed menu appearance and simplified code.

--- quartz/components/Navbar.tsx
+++ quartz/components/Navbar.tsx
@@ -4,77 +4,11 @@ import navbarStyle from "./styles/navbar.scss"
 
 // @ts-ignore
 import script from "./scripts/navbar.inline"
-import { NavbarNode, FileNode, Options } from "./NavbarNode"
-import { QuartzPluginData } from "../plugins/vfile"
+import { Options } from "./NavbarNode"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
 
-// Options interface defined in `NavbarNode` to avoid circular dependency
-const defaultOptions = {
-  folderClickBehavior: "collapse",
-  folderDefaultState: "open",
-  useSavedState: true,
-  mapFn: (node) => {
-    return node
-  },
-  sortFn: (a, b) => {
-    // Sort order: folders first, then files. Sort folders and files alphabetically
-    if ((!a.file && !b.file) || (a.file && b.file)) {
-      // numeric: true: Whether numeric collation ehould be used, such that "1" < "2" < "10"
-      // sensitivity: "base": Only strings that differ in base letters compare as unequal. Examples: a ≠ b, a = á, a = A
-      return a.displayName.localeCompare(b.displayName, undefined, {
-        numeric: true,
-        sensitivity: "base",
-      })
-    }
-
-    if (a.file && !b.file) {
-      return 1
-    } else {
-      return -1
-    }
-  },
-  filterFn: (node) => node.name !== "tags",
-  order: ["filter", "map", "sort"],
-} satisfies Options
-
 export default ((userOpts?: Partial<Options>) => {
-  // Parse config
-  const opts: Options = { ...defaultOptions, ...userOpts }
-
-  // memoized
-  let fileTree: FileNode
-  let jsonTree: string
-
-  function constructFileTree(allFiles: QuartzPluginData[]) {
-    if (fileTree) {
-      return
-    }
-
-    // Construct tree from allFiles
-    fileTree = new FileNode("")
-    allFiles.forEach((file) => fileTree.add(file))
-
-    // Execute all functions (sort, filter, map) that were provided (if none were provided, only default "sort" is applied)
-    if (opts.order) {
-      for (let i = 0; i < opts.order.length; i++) {
-        const functionName = opts.order[i]
-        if (functionName === "map") {
-          fileTree.map(opts.mapFn)
-        } else if (functionName === "sort") {
-          fileTree.sort(opts.sortFn)
-        } else if (functionName === "filter") {
-          fileTree.filter(opts.filterFn)
-        }
-      }
-    }
-
-    // Get all folders of tree. Initialize with collapsed state
-    // Stringify to pass json tree as data attribute ([data-tree])
-    const folders = fileTree.getFolderPaths(opts.folderDefaultState === "collapsed")
-    jsonTree = JSON.stringify(folders)
-  }
-
   const Navbar: QuartzComponent = ({
     cfg,
     allFiles,
@@ -86,7 +20,7 @@ export default ((userOpts?: Partial<Options>) => {
     const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
     const baseDir = pathToRoot(fileData.slug!)
     const pageTitle = (
-      <div class={classNames(displayClass, "page-title")} style="margin:0">
+      <div class={classNames(displayClass, "page-title")}>
         <img src={"../static/pond.gif"} class="header-img no-select"></img>
         <h2 class="page-title-text">
           <a href={baseDir} id="page-title-text">
@@ -96,36 +30,37 @@ export default ((userOpts?: Partial<Options>) => {
       </div>
     )
 
+    const links = pages.map((page) => (
+      <li key={page.slug}>
+        <a href={page.slug}>{page.title}</a>
+      </li>
+    ))
+    const pageLinks = (
+      <nav className="menu">
+        <ul>{links}</ul>
+      </nav>
+    )
+    // const pageLinksDesktop = (
+    //   <nav className="menu" id="desktop-nav">
+    //     <ul>{links}</ul>
+    //   </nav>
+    // )
     return (
       <div className={classNames(displayClass, "navbar")}>
-        <div id="navbar-content" className="desktop-only">
-          {pageTitle}
-          <ul className="overflow desktop-only" id="navbar-ul">
-            {pages.map((page) => (
-              <li key={page.slug}>
-                <a href={page.slug}>{page.title}</a>
-              </li>
-            ))}
-          </ul>
-        </div>
+        {pageTitle}
+        {/* <div className="desktop-only"> */}
+        {/*   {pageLinks} */}
+        {/* </div> */}
         <button className="hamburger mobile-only">
           <span />
           <span />
           <span />
         </button>
+        {pageLinks}
 
-        <div className="mobile-only">
-          {pageTitle}
-          <nav className="mobile-only menu">
-            <ul className="overflow">
-              {pages.map((page) => (
-                <li key={page.slug}>
-                  <a href={page.slug}>{page.title}</a>
-                </li>
-              ))}
-            </ul>
-          </nav>
-        </div>
+        {/* <div className="mobile-only"> */}
+        {/*   {pageLinks} */}
+        {/* </div> */}
       </div>
     )
   }
