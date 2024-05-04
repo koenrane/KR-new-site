5f189fe6cd53896319c9381f0495a1de972f7cfb
Alex Turner <turneral1@grinnell.edu>
2024-05-04 14:40:11 -0700

    --amend

--- quartz/static/scripts/detect-dark-mode.js
+++ quartz/static/scripts/detect-dark-mode.js
@@ -3,13 +3,4 @@
   const userPref = window.matchMedia("(prefers-color-scheme: light)").matches ? "light" : "dark"
   const currentTheme = localStorage.getItem("theme") ?? userPref
   document.documentElement.setAttribute("saved-theme", currentTheme) // Using data-theme for clarity and custom attribute handling
-
-  // Emit theme change event if needed (you can define this function in your external JS if it's not used immediately)
-  const emitThemeChangeEvent = (theme) => {
-    const event = new CustomEvent("themechange", { detail: { theme } })
-    document.dispatchEvent(event)
-  }
-
-  // If needed right away, you can call emitThemeChangeEvent(currentTheme);
-  emitThemeChangeEvent(currentTheme)
 })()
