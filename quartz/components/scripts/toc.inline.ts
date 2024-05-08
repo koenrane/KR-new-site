98f11a4e79192d102385ebd57828248f0ef3334a
Alex Turner <turneral1@grinnell.edu>
2024-05-08 10:02:02 -0700

    Fixed bug where multiple TOC sections would show as active at the same time

--- quartz/components/scripts/toc.inline.ts
+++ quartz/components/scripts/toc.inline.ts
@@ -1,7 +1,5 @@
 document.addEventListener("DOMContentLoaded", function () {
-  const sections = document.querySelectorAll(
-    ".center h1, .center h2, .center h3, .center h4, .center h5, .center h6",
-  )
+  const sections = document.querySelectorAll(".center h1, .center h2, .center h3")
   const navLinks = document.querySelectorAll("#toc-content a")
 
   function onScroll() {
@@ -9,14 +7,15 @@ document.addEventListener("DOMContentLoaded", function () {
 
     sections.forEach((section) => {
       const sectionTop = section.offsetTop
-      if (scrollY >= sectionTop - 100) {
+      console.log(sectionTop)
+      if (scrollY + 300 >= sectionTop) {
         currentSection = section.getAttribute("id")
       }
     })
 
     navLinks.forEach((link) => {
       link.classList.remove("active")
-      if (currentSection && link.href.includes(currentSection)) {
+      if (currentSection && link.href.endsWith(currentSection)) {
         link.classList.add("active")
       }
     })
