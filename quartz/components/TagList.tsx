624c0701db8c0a6e38378696248d25e3ad504ab2
Alex Turner <turneral1@grinnell.edu>
2024-05-13 21:35:34 -0700

    Reworked Tags to allow multi word tags, displayed in descending order by length

--- quartz/components/TagList.tsx
+++ quartz/components/TagList.tsx
@@ -3,16 +3,21 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import { classNames } from "../util/lang"
 
 const TagList: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
-  const tags = fileData.frontmatter?.tags
+  let tags = fileData.frontmatter?.tags || []
+  // Sort by string lenth, descending
+  tags = tags.sort((a, b) => b.length - a.length)
   const baseDir = pathToRoot(fileData.slug!)
   if (tags && tags.length > 0) {
     return (
       <>
         <div>
-          {/* <p style="margin: 0; font-size: var(--text-size-90)">Tags</p> */}
           <ul class={classNames(displayClass, "tags")}>
             {tags.map((tag) => {
               const linkDest = baseDir + `/tags/${slugTag(tag)}`
+              tag = tag.replace(/-/g, " ").toLowerCase()
+              if (tag === "AI") {
+                tag = "AI"
+              }
               return (
                 <a href={linkDest} class="internal tag-link">
                   {tag}
