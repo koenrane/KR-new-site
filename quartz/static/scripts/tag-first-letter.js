6ecbb3a8041fb593f741e9c84d45501dc3be4d15
Alex Turner <turneral1@grinnell.edu>
2024-05-23 11:49:36 -0700

    Drop cap now loads after navigating from search

--- quartz/static/scripts/tag-first-letter.js
+++ quartz/static/scripts/tag-first-letter.js
@@ -1,5 +1,31 @@
+// Function to update the first paragraph
+function updateFirstParagraph() {
+  const paragraphs = document.querySelectorAll("article > p:first-of-type")
+  paragraphs.forEach((paragraph) => {
+    const firstLetter = paragraph.textContent.trim().charAt(0)
+    paragraph.setAttribute("data-first-letter", firstLetter)
+  })
+}
 document.addEventListener("DOMContentLoaded", function () {
-  const firstParagraph = document.querySelector("article > p:first-of-type")
-  const firstLetter = firstParagraph.textContent.trim().charAt(0)
-  firstParagraph.setAttribute("data-first-letter", firstLetter)
+  // Call the function initially to set the attribute on page load
+  updateFirstParagraph()
+
+  // Create a MutationObserver to watch for changes in the DOM
+  const observer = new MutationObserver((mutationsList, observer) => {
+    for (let mutation of mutationsList) {
+      if (
+        mutation.type === "childList" ||
+        mutation.type === "subtree" ||
+        mutation.type === "characterData"
+      ) {
+        updateFirstParagraph()
+      }
+    }
+  })
+
+  // Configuration for the observer
+  const config = { childList: true, subtree: true, characterData: true }
+
+  // Start observing the target node for configured mutations
+  observer.observe(document.body, config)
 })
