76f025b9f1a5b2110ad5b4f37cd07c9edbcd76b1
Alex Turner <turneral1@grinnell.edu>
2024-06-04 17:12:52 -0700

    Added more backstop configs, version controlling the reference images

--- backstop.cjs
+++ backstop.cjs
@@ -1,35 +1,45 @@
-const scenarios = require('./backstop/scenarios.cjs');
+const scenarios = require("./backstop/scenarios.cjs")
 
 module.exports = {
-  "id": "backstop_default",
-  "viewports": [
+  id: "backstop_default",
+  viewports: [
     {
-      "label": "phone",
-      "width": 320,
-      "height": 480
+      label: "phone",
+      width: 320,
+      height: 480,
     },
     {
-      "label": "tablet",
-      "width": 1024,
-      "height": 768
-    }
+      label: "tablet",
+      width: 1024,
+      height: 768,
+    },
+    {
+      label: "1080p",
+      width: 1920,
+      height: 1080,
+    },
+    {
+      label: "4K",
+      width: 4096,
+      height: 2160,
+    },
   ],
-  "scenarios": scenarios,
-    "onReadyScript": "puppet/onReady.cjs",
-  "paths": {
-"bitmaps_reference": "backstop/backstop_data/bitmaps_reference",
-    "bitmaps_test": "backstop/backstop_data/bitmaps_test",
-    "engine_scripts": "backstop/backstop_data/engine_scripts",
-    "html_report": "backstop/backstop_data/html_report",
-    "ci_report": "backstop/backstop_data/ci_report"
+  scenarios: scenarios,
+  onReadyScript: "puppet/onReady.cjs",
+  paths: {
+    bitmaps_reference: "backstop/backstop_data/bitmaps_reference",
+    bitmaps_test: "backstop/backstop_data/bitmaps_test",
+    engine_scripts: "backstop/backstop_data/engine_scripts",
+    html_report: "backstop/backstop_data/html_report",
+    ci_report: "backstop/backstop_data/ci_report",
   },
-  "report": ["browser"],
-  "engine": "puppeteer",
-  "engineOptions": {
-    "args": ["--no-sandbox"]
+  report: ["browser"],
+  engine: "puppeteer",
+  engineOptions: {
+    args: ["--no-sandbox"],
   },
-  "asyncCaptureLimit": 5,
-  "asyncCompareLimit": 50,
-  "debug": false,
-  "debugWindow": false
+  asyncCaptureLimit: 5,
+  asyncCompareLimit: 50,
+  debug: false,
+  debugWindow: false,
 }
