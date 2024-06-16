d891e3400643b86cbefe7d6cbd0ff835fa13ca61
Alex Turner <turneral1@grinnell.edu>
2024-06-16 16:24:07 -0700

    Now hiding mp4s and webms in backstop testing

--- backstop/backstop_data/engine_scripts/puppet/onReady.cjs
+++ backstop/backstop_data/engine_scripts/puppet/onReady.cjs
@@ -1,14 +1,17 @@
 // backstop_data/engine_scripts/puppet/onReady.cjs
 const clickAndHoverHelper = require("./clickAndHoverHelper.cjs")
 
-module.exports = async (page, scenario, vp) => {
+module.exports = async (page, scenario) => {
   await page.waitForSelector("body", { timeout: 5000 })
   clickAndHoverHelper(page, scenario)
 
-  // Add a delay to wait for a specific frame -- synchronize GIFs
+  await page.waitForSelector("#header-video") // TODO kinda hacky
   await page.evaluate(() => {
-    document.querySelectorAll("img").forEach((img) => {
-      if (img.src.endsWith(".gif")) {
+    const hideMediaTypes = [".gif", ".mp4", ".webm"]
+
+    document.querySelectorAll("img, video").forEach((img) => {
+      const src = img?.src || img?.children[0]?.src
+      if (hideMediaTypes.some((ext) => src.endsWith(ext))) {
         img.style.visibility = "hidden"
       }
     })
