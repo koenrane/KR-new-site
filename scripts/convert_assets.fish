7c54f59621111c9c1032cd0a6e4f355bf4b14c3c
Alex Turner <turneral1@grinnell.edu>
2024-07-02 14:41:03 -0700

    Updated scripts to fish for greater readability. Fixed several broken pieces, should now be updating references properly.

---
+++ scripts/convert_assets.fish
@@ -0,0 +1,53 @@
+#!/usr/bin/env fish
+
+# Initialize argparse
+argparse 'r/remove_originals' 's/strip_metadata' -- $argv
+
+# Function to handle conversion and optimization of a single file
+function convert_asset
+    set input_file $argv[1]
+    set output_file
+
+    switch $input_file
+        case '*.jpg' '*.jpeg' '*.png'
+            sh scripts/convert_to_avif.sh $input_file
+            if test $status -ne 0
+                return 1
+            end
+            set output_file (string replace -r '\.[^.]+$' '.avif' $input_file)
+            fish scripts/update_references.fish --source $input_file --target $output_file
+
+        case '*.gif' '*.mp4' '*.mov'
+            set output_file (string replace -r '\.[^.]+$' '.webm' $input_file)
+            sh scripts/convert_to_webm.sh $input_file
+            # Has its own reference replacement
+
+        case '*.flac'
+            # Example: Audio Compression (FLAC to Opus)
+            # set output_file (string replace -r '\.[^.]+$' '.opus' $input_file)
+            # ffmpeg -i $input_file $output_file
+
+        case '*'
+            return 0
+    end
+
+    # Strip Metadata (If --strip_metadata flag is provided)
+    if set -q _flag_strip_metadata
+        exiftool -all= $output_file
+    end
+
+    # Remove Original File (If --remove_originals flag is provided)
+    if set -q _flag_remove_originals -a -f $output_file
+        rm $input_file
+    end
+
+    set_color green
+    echo "Processed $input_file"
+    set_color normal
+end
+
+# Traverse through all files in the specified directory and subdirectories
+find quartz/static -type f | while read -l file
+    convert_asset $file
+end
+
--- scripts/process_file.fish
+++
@@ -1,4 +0,0 @@
-set -l file $argv[1]
-
-fish download_images.fish $file
-bash convert_footnotes.sh $file
