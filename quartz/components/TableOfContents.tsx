08dad4b89bc29db9efdfba2c6820be7243b8728b
Alex Turner <alex@turntrout.com>
2024-08-08 17:32:19 -0700

    Simplified TOC styling

--- quartz/components/TableOfContents.tsx
+++ quartz/components/TableOfContents.tsx
@@ -1,19 +1,9 @@
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
-import legacyStyle from "./styles/legacyToc.scss"
 import modernStyle from "./styles/toc.scss"
 import { classNames } from "../util/lang"
 
 // @ts-ignore
 import script from "./scripts/toc.inline"
-import { i18n } from "../i18n"
-
-interface Options {
-  layout: "modern" | "legacy"
-}
-
-const defaultOptions: Options = {
-  layout: "modern",
-}
 
 const TableOfContents: QuartzComponent = ({ fileData, displayClass }: QuartzComponentProps) => {
   if (!fileData.toc || fileData.frontmatter?.toc === "false") {
@@ -68,30 +58,6 @@ function addListItem(remainingEntries, currentDepth: number) {
 TableOfContents.css = modernStyle
 TableOfContents.afterDOMLoaded = script
 
-const LegacyTableOfContents: QuartzComponent = ({ fileData, cfg }: QuartzComponentProps) => {
-  if (!fileData.toc) {
-    return null
-  }
-  return (
-    <details id="toc" open={!fileData.collapseToc}>
-      <summary>
-        <h3>{i18n(cfg.locale).components.tableOfContents.title}</h3>
-      </summary>
-      <ul>
-        {fileData.toc.map((tocEntry) => (
-          <li key={tocEntry.slug} class={`depth-${tocEntry.depth}`}>
-            <a href={`#${tocEntry.slug}`} data-for={tocEntry.slug}>
-              {tocEntry.text}
-            </a>
-          </li>
-        ))}
-      </ul>
-    </details>
-  )
-}
-LegacyTableOfContents.css = legacyStyle
-
-export default ((opts?: Partial<Options>) => {
-  const layout = opts?.layout ?? defaultOptions.layout
-  return layout === "modern" ? TableOfContents : LegacyTableOfContents
+export default ((_opts?) => {
+  return TableOfContents
 }) satisfies QuartzComponentConstructor
