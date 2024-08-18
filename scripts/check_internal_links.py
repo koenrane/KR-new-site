f9f2c2deaf9b0349195567f2128b59a14578b65a
Alex Turner <alex@turntrout.com>
2024-08-17 21:45:16 -0700

    Improve internal link checker 💪

--- scripts/check_internal_links.py
+++ scripts/check_internal_links.py
@@ -1,19 +1,14 @@
 import subprocess
-import json
-import requests
 
 
-def run_linkchecker(url):
+def run_linkchecker(url_pattern: str = "public/**html"):
     try:
         result = subprocess.run(
             [
                 "linkchecker",
                 # "--check-extern",  # Check both internal and external links
-                "--recursion-level=1",
-                "--no-status",
                 "--ignore-url=^mailto:",
-                "--output=csv",
-                url,
+                url_pattern,
             ],
             capture_output=True,
             text=True,
@@ -25,39 +20,11 @@ def run_linkchecker(url):
         return []
 
 
-def check_links(base_url):
-    results = run_linkchecker(base_url)
-    internal_errors = []
-    print(results)
-
-    for line in results[1:]:  # Skip the header line
-        parts = line.split(";")
-        if len(parts) >= 8 and parts[0] != "Valid":
-            url = parts[1]
-            parent_url = parts[2]
-            error_message = parts[7]
-            if url.startswith(base_url):
-                internal_errors.append(
-                    {
-                        "url": url,
-                        "parent_url": parent_url,
-                        "info": error_message,
-                    }
-                )
-
-    return internal_errors
-
-
 def main():
-    base_url = "http://localhost:8080"  # Replace with your local development server URL
-
-    print(f"Checking internal links for {base_url}")
-    errors = check_links(base_url)
+    errors = run_linkchecker()
 
     if errors:
-        print("Internal link errors found:")
-        for error in errors:
-            print(f"- {error['url']}: {error['info']}")
+        print(f"Internal link errors found: {errors}")
     else:
         print("No internal link errors found.")
 
