baaf1788b0fd289cc72033ca509f60ca8c263ad5
Alex Turner <alex@turntrout.com>
2024-09-02 19:46:03 -1000

    Fix(linkchecker): Take arguments from command line
    - The linkchecker now takes an optional list of files to check
    - Updated tests to reflect changes in error message format

--- scripts/linkchecker.fish
+++ scripts/linkchecker.fish
@@ -1,13 +1,19 @@
+#!/usr/bin/env fish
+
 # If there are no arguments passed, then default to the GIT_ROOT public
-set -l GIT_ROOT $(git rev-parse --show-toplevel)
-set -l TARGET_FILES [$GIT_ROOT/public/**html] #TODO make this take an argument
+set -l GIT_ROOT (git rev-parse --show-toplevel)
+set -l TARGET_FILES $argv
+
+if test -z "$TARGET_FILES"
+    set TARGET_FILES $GIT_ROOT/public/**html
+end
 
 # So, the ignore-url syntax is very confusing. 
 #  linkchecker only checks those links which *match* the regex. 
 #  So if you want to ignore a link, you need to match it with a regex that doesn't match the link.
 
 # Internal links should NEVER 404!
-linkchecker $GIT_ROOT/public/**html --ignore-url="!^\.+.*" --no-warnings
+linkchecker $TARGET_FILES --ignore-url="!^\.+.*" --no-warnings
 
 # CDN links should never 404
-linkchecker $GIT_ROOT/public/**html --ignore-url="!^https?://assets\.turntrout\.com" --no-warnings --check-extern
+linkchecker $TARGET_FILES --ignore-url="!^https?://assets\.turntrout\.com" --no-warnings --check-extern
--- scripts/tests/test_linkchecker.py
+++ scripts/tests/test_linkchecker.py
@@ -11,19 +11,19 @@ def linkchecker_result():
 
     # Run the linkchecker script and capture its output
     result = subprocess.run(["fish", git_root + "/scripts/linkchecker.fish", 
-    "--files", Path(git_root, "scripts", "tests", ".linkchecker.test.html")], 
+   Path(git_root, "scripts", "tests", ".linkchecker.test.html")], 
                             capture_output=True, 
                             text=True, 
                             check=False) # Expect this to error
     return result
 
 def test_invalid_port_error(linkchecker_result):
-    assert "Linkchecker: Found invalid port error" in linkchecker_result.stderr, "Invalid port error not found in output"
+    assert "Error: URL is unrecognized or has invalid syntax" in linkchecker_result.stdout, "Invalid port error not found in output"
 
     assert linkchecker_result.returncode == 1, f"Linkchecker script failed with return code {linkchecker_result.returncode}"
 
 def test_invalid_asset_error(linkchecker_result):
-    assert "Linkchecker: Found invalid asset error" in linkchecker_result.stderr, "Invalid asset error not found in output"
+    assert "Error: 404 Not Found" in linkchecker_result.stdout, "Invalid asset error not found in output"
     assert linkchecker_result.returncode == 1, f"Linkchecker script failed with return code {linkchecker_result.returncode}"
 
 if __name__ == "__main__":
