0515241b914dcf7293806db3755610ebfd839f55
Alex Turner <alex@turntrout.com>
2024-09-02 17:03:57 -1000

    Improved metadata in HEAD, incomplete integration of dropcap CSS filtering

--- quartz/components/Head.tsx
+++ quartz/components/Head.tsx
@@ -1,5 +1,4 @@
 import { i18n } from "../i18n"
-import { FullSlug, joinSegments, pathToRoot } from "../util/path"
 import { JSResourceToScriptElement } from "../util/resources"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { formatTitle } from "./component_utils"
@@ -12,36 +11,54 @@ export default (() => {
       fileData.description?.trim() ?? i18n(cfg.locale).propertyDefaults.description
     const { css, js } = externalResources
 
-    const url = new URL(`https://${cfg.baseUrl ?? "turntrout.com"}`)
-    const path = url.pathname as FullSlug
-    const baseDir = fileData.slug === "404" ? path : pathToRoot(fileData.slug!)
+    // Reconstruct the URL for this page (its permalink)
+    const url = new URL(`https://${cfg.baseUrl ?? "turntrout.com"}/${fileData.slug}`)
 
-    const iconPath = joinSegments(baseDir, "static/images/favicon.ico")
+    const iconPath = "static/images/favicon.ico"
     const permalink = fileData.permalink || url.href
-    const siteImage = joinSegments("https://assets.turntrout.com/static/pond.webm")
+    const siteImage = "https://assets.turntrout.com/static/pond.webm"
+
+    let mediaElement = (
+      <>
+        <meta property="og:image" content={siteImage} />
+        <meta
+          property="og:image:alt"
+          content="A trout and a goose playing in a pond, with a castle in the background."
+        />
+      </>
+    )
+
+    if (fileData?.video_preview_link) {
+      mediaElement = <meta property="og:video" content={fileData.video_preview_link as string} />
+      // TODO alt element?
+    }
+
+    let cssToUse = css
+    // if (fileData?.no_dropcap) {
+    //     cssToUse = css.filter() // TODO have Cursor help me with this
+    // }
 
     return (
       <head>
         <title>{title}</title>
+        <meta name="description" content={description} />
+
         <meta charSet="utf-8" />
         <script src="/static/scripts/detect-dark-mode.js"></script>
         <script src="/static/scripts/tag-first-letter.js"></script>
         <script src="/static/scripts/collapsible-listeners.js"></script>
         <script src="/static/scripts/DOMContentLoaded.js"></script>
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
         <meta property="og:title" content={title} />
-        <meta property="og:url" content={permalink} />
+        <meta property="og:type" content="article" />
+        <meta property="og:url" content={permalink as string} />
         <meta property="og:site_name" content="The Pond" />
         <meta property="og:description" content={description} />
-        <meta property="og:image" content={siteImage} />
-        <meta
-          property="og:image:alt"
-          content="A trout and a goose playing in a pond, with a castle in the background."
-        />
+        {mediaElement}
+
         <link rel="icon" href={iconPath} />
-        <meta name="description" content={description} />
-        <meta name="generator" content="Quartz" />
-        {css.map((href) => (
+        {cssToUse.map((href) => (
           <link key={href} href={href} rel="stylesheet" type="text/css" spa-preserve />
         ))}
         {js
