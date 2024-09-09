d9bfe82b407cb301cdca5530a1724967785c4b70
Alex Turner <alex@turntrout.com>
2024-09-08 20:30:27 -0700

    Now formatting titles correctly with double quotes

--- quartz/components/component_utils.tsx
+++ quartz/components/component_utils.tsx
@@ -1,13 +1,11 @@
 import { titleCase } from "title-case"
-import { niceQuotes } from "../plugins/transformers/formatting_improvement_html"
+import { applyTextTransforms } from "../plugins/transformers/formatting_improvement_html"
 
-// Replace single quotes with double quotes for consistency
-// NOTE this is a bit of a hack, but probably fine
 export function formatTitle(title: string): string {
-  title = niceQuotes(title)
-  if (title.includes("‘") && title.includes("’")) {
-    title = title.replace(/(?<= |^)‘/g, "“").replace(/’(?<= |$)/g, "”")
-  }
+  // Replace single quotes with double quotes for consistency
+  title = title.replace(/( |^)\'/g, '$1"').replace(/\'([ \?\!\.]|$)/g, '"$1')
+  title = applyTextTransforms(title)
+
   // Convert title to title case
   title = titleCase(title, { locale: "en-US" })
   return title
