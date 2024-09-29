e6c5b62f25e9d377cb09b44e9f0569a3a52c483b
Alex Turner <alex@turntrout.com>
2024-09-29 11:32:25 -0700

    - Replaces `images_dir` with `asset_dir` for consistency. - Now renaming cloudinary files properly, without duplicating the extension

--- scripts/download_images.fish
+++ scripts/download_images.fish
@@ -1,4 +1,4 @@
-# We want to host all images locally
+# We want to host all assets locally
 function download_asset
     set -l url $argv[1]
     set -l filename (basename $url)
@@ -12,13 +12,13 @@ end
 
 # --- Main Logic ---
 
-function main --description 'Download images from Markdown files and replace URLs'
+function main --description 'Download assets from Markdown files and replace URLs'
     set -l markdown_files $argv
-    set -l images_dir quartz/static/images/posts
+    set -l asset_dir quartz/static/images/posts
     set -l target_dir static/images/posts
 
-    # 1. Find all image URLs in Markdown files
-    set -l image_urls (command grep -oE --no-filename 'http[^\)]*?\.(jpe?g|png|webp)' $markdown_files)
+    # 1. Find all asset URLs in Markdown files
+    set -l asset_urls (command grep -oE --no-filename 'http[^\)]*?\.(jpe?g|png|webp|gif|mpeg|avi(?=\))|webm)' $markdown_files)
 
     # Add in mp4 files which don't start with https://assets.turntrout
     set -l mp4_regex 'http[^\)]*?\.mp4'
@@ -38,16 +38,22 @@ function main --description 'Download images from Markdown files and replace URL
     echo $cloudinary_urls
 
     for url in $cloudinary_urls
-        download_asset $url $asset_dir
+        set -g filename (basename $url)
 
-        # Detect what filetype it is 
-        set -l filetype (file --mime-type -b $asset_dir/$(basename $url) | cut -d'/' -f2)
-        # Mv the asset so it has a webp extension
-        mv $asset_dir/$(basename $url){,.$filetype}
+        download_asset $url $asset_dir
+        
+        set -l extension (string match -r '\.[^.]+$' $filename)
+        if test -z "$extension" # No extension
+            set -l file_type (file --mime-type -b $asset_dir/$filename)
+            set -l detected_extension (echo $file_type | sed 's|^.*/||')
+
+            mv  "$asset_dir/$filename"  "$asset_dir/$filename.$detected_extension"
+            set -g filename "$filename.$detected_extension"
+            echo "Downloaded and renamed: $filename"
+        end
 
         set -l escaped_url (echo $url | sed 's|[/\\.^$\[\]]|\\&|g')
-        # These are webp so make the extension explicit
-        sed -i ''.bak "s|$escaped_url|/$target_dir/$(basename $url).$filetype|g" $markdown_files
+        sed -i ''.bak "s|$escaped_url|/$target_dir/$filename|g" $markdown_files
     end
 
     echo "Asset download and replacement complete!"
