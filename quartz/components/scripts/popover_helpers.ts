e76ec7d829b2ab0252f7269a598658cafb7999e6
Alex Turner <alex@turntrout.com>
2024-10-11 09:24:20 -0700

    Fixed popover tests

--- quartz/components/scripts/popover_helpers.ts
+++ quartz/components/scripts/popover_helpers.ts
@@ -1,9 +1,10 @@
 import { normalizeRelativeURLs } from "../../util/path"
 
 export interface PopoverOptions {
-    parentElement: HTMLElement;
-    targetUrl: URL;
-    linkElement: HTMLLinkElement;
+  parentElement: HTMLElement
+  targetUrl: URL
+  linkElement: HTMLLinkElement
+  customFetch?: typeof fetch // Add this line
 }
 
 const parser = new DOMParser()
@@ -13,74 +14,72 @@ const parser = new DOMParser()
  * @param options - The options for creating the popover
  * @returns A Promise that resolves to the created popover element
  */
-export async function createPopover(options: PopoverOptions): Promise<HTMLElement | null> {
-    const { targetUrl, linkElement } = options;
+export async function createPopover(options: PopoverOptions): Promise<HTMLElement> {
+  const { targetUrl, linkElement, customFetch = fetch } = options
 
-    // Check if the link is a footnote back arrow
-    const footnoteRefRegex = /^#user-content-fnref-\d+$/;
-    if (footnoteRefRegex.test(linkElement.getAttribute('href') || '')) {
-        return null;
-    }
+  // Check if the link is a footnote back arrow
+  const footnoteRefRegex = /^#user-content-fnref-\d+$/
+  if (footnoteRefRegex.test(linkElement.getAttribute("href") || "")) {
+    throw new Error("Footnote back arrow links are not supported for popovers")
+  }
 
-    const popoverElement = document.createElement("div");
-    popoverElement.classList.add("popover");
-    const popoverInner = document.createElement("div");
-    popoverInner.classList.add("popover-inner");
-    popoverElement.appendChild(popoverInner);
+  const popoverElement = document.createElement("div")
+  popoverElement.classList.add("popover")
+  const popoverInner = document.createElement("div")
+  popoverInner.classList.add("popover-inner")
+  popoverElement.appendChild(popoverInner)
 
-    try {
-        const response = await fetch(`${targetUrl}`);
-        if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        const contentType = response.headers.get("Content-Type");
-        if (!contentType) throw new Error("No content type received");
+  const response = await customFetch(`${targetUrl}`)
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`)
+  }
 
-        const [contentTypeCategory, typeInfo] = contentType.split("/");
-        popoverInner.dataset.contentType = contentType;
+  const contentType = response.headers.get("Content-Type")
+  if (!contentType) throw new Error("No content type received")
 
-        switch (contentTypeCategory) {
-            case "image":
-                const img = document.createElement("img");
-                img.src = targetUrl.toString();
-                img.alt = targetUrl.pathname;
-                popoverInner.appendChild(img);
-                break;
-            case "application":
-                if (typeInfo === "pdf") {
-                    const pdf = document.createElement("iframe");
-                    pdf.src = targetUrl.toString();
-                    popoverInner.appendChild(pdf);
-                }
-                break;
-            default:
-                const contents = await response.text();
-                const html = parser.parseFromString(contents, "text/html");
-                normalizeRelativeURLs(html, targetUrl);
+  const [contentTypeCategory, typeInfo] = contentType.split("/")
+  popoverInner.dataset.contentType = contentType
 
-                const hintElements = html.getElementsByClassName("popover-hint");
-                Array.from(hintElements).forEach(elt => {
-                    const popoverHeadings = elt.querySelectorAll('h1, h2, h3, h4, h5, h6, li');
-                    popoverHeadings.forEach(element => {
-                        if (element.id) {
-                            element.id = `${element.id}-popover`;
-                        }
-                    });
-                    popoverInner.appendChild(elt);
-                });
-        }
-    } catch (error) {
-        console.error("Error creating popover:", error);
-        popoverInner.textContent = "Failed to load content";
-        return popoverElement; // Return the popover element with error message
-    }
+  let img: HTMLImageElement | null = null
+  let contents: string | null = null
+  let html: Document | null = null
+  let hintElements: Element[] = []
+  switch (contentTypeCategory) {
+    case "image":
+      img = document.createElement("img")
+      img.src = targetUrl.toString()
+      img.alt = targetUrl.pathname
+      popoverInner.appendChild(img)
+      break
+    case "application":
+      if (typeInfo === "pdf") {
+        const pdf = document.createElement("iframe")
+        pdf.src = targetUrl.toString()
+        popoverInner.appendChild(pdf)
+      }
+      break
+    default:
+      contents = await response.text()
+      html = parser.parseFromString(contents, "text/html")
+      normalizeRelativeURLs(html, targetUrl)
 
-    return popoverElement;
+      hintElements = Array.from(html.getElementsByClassName("popover-hint"))
+      Array.from(hintElements).forEach((elt) => {
+        const popoverHeadings = elt.querySelectorAll("h1, h2, h3, h4, h5, h6, li")
+        popoverHeadings.forEach((element) => {
+          if (element.id) {
+            element.id = `${element.id}-popover`
+          }
+        })
+        popoverInner.appendChild(elt)
+      })
+  }
+
+  return popoverElement
 }
 
-
 // POSITIONING the popover
-export const POPOVER_PADDING = 5;
+export const POPOVER_PADDING = 5
 
 /**
  * Computes the left position of the popover
@@ -89,13 +88,13 @@ export const POPOVER_PADDING = 5;
  * @returns The computed left position
  */
 export function computeLeft(linkRect: DOMRect, popoverWidth: number): number {
-    const initialLeft = linkRect.left - popoverWidth - POPOVER_PADDING;
+  const initialLeft = linkRect.left - popoverWidth - POPOVER_PADDING
 
-    // Ensure the popover doesn't go off the left or right edge of the screen
-    const maxLeft = window.innerWidth - popoverWidth - POPOVER_PADDING;
-    const minLeft = POPOVER_PADDING;
+  // Ensure the popover doesn't go off the left or right edge of the screen
+  const maxLeft = window.innerWidth - popoverWidth - POPOVER_PADDING
+  const minLeft = POPOVER_PADDING
 
-    return Math.max(minLeft, Math.min(initialLeft, maxLeft));
+  return Math.max(minLeft, Math.min(initialLeft, maxLeft))
 }
 
 /**
@@ -105,14 +104,14 @@ export function computeLeft(linkRect: DOMRect, popoverWidth: number): number {
  * @returns The computed top position
  */
 export function computeTop(linkRect: DOMRect, popoverHeight: number): number {
-    // Calculate top position to be centered vertically with the link
-    const initialTop = 0.5 * (linkRect.top + linkRect.bottom) - 0.5 * popoverHeight + window.scrollY;
+  // Calculate top position to be centered vertically with the link
+  const initialTop = 0.5 * (linkRect.top + linkRect.bottom) - 0.5 * popoverHeight + window.scrollY
 
-    // Ensure the popover doesn't go off the top or bottom of the screen
-    const minTop = window.scrollY + POPOVER_PADDING;
-    const maxTop = window.scrollY + window.innerHeight - popoverHeight - POPOVER_PADDING;
+  // Ensure the popover doesn't go off the top or bottom of the screen
+  const minTop = window.scrollY + POPOVER_PADDING
+  const maxTop = window.scrollY + window.innerHeight - popoverHeight - POPOVER_PADDING
 
-    return Math.max(minTop, Math.min(initialTop, maxTop));
+  return Math.max(minTop, Math.min(initialTop, maxTop))
 }
 
 /**
@@ -120,19 +119,22 @@ export function computeTop(linkRect: DOMRect, popoverHeight: number): number {
  * @param popoverElement - The popover element to position
  * @param linkElement - The link element to position relative to
  */
-export function setPopoverPosition(popoverElement: HTMLElement, linkElement: HTMLLinkElement): void {
-    const linkRect = linkElement.getBoundingClientRect();
-    const popoverWidth = popoverElement.offsetWidth;
-    const popoverHeight = popoverElement.offsetHeight;
+export function setPopoverPosition(
+  popoverElement: HTMLElement,
+  linkElement: HTMLLinkElement,
+): void {
+  const linkRect = linkElement.getBoundingClientRect()
+  const popoverWidth = popoverElement.offsetWidth
+  const popoverHeight = popoverElement.offsetHeight
 
-    const left = computeLeft(linkRect, popoverWidth);
-    const top = computeTop(linkRect, popoverHeight);
+  const left = computeLeft(linkRect, popoverWidth)
+  const top = computeTop(linkRect, popoverHeight)
 
-    Object.assign(popoverElement.style, {
-        position: "absolute",
-        left: `${left}px`,
-        top: `${top}px`,
-    });
+  Object.assign(popoverElement.style, {
+    position: "absolute",
+    left: `${left}px`,
+    top: `${top}px`,
+  })
 }
 
 /**
@@ -141,62 +143,67 @@ export function setPopoverPosition(popoverElement: HTMLElement, linkElement: HTM
  * @param linkElement - The link element
  * @returns A cleanup function to remove the event listeners
  */
-export function attachPopoverEventListeners(popoverElement: HTMLElement, linkElement: HTMLLinkElement): () => void {
-    let isMouseOverLink = false;
-    let isMouseOverPopover = false;
+export function attachPopoverEventListeners(
+  popoverElement: HTMLElement,
+  linkElement: HTMLLinkElement,
+): () => void {
+  let isMouseOverLink = false
+  let isMouseOverPopover = false
 
-    const removePopover = () => {
-        popoverElement.classList.remove("visible");
-        setTimeout(() => {
-            if (!isMouseOverLink && !isMouseOverPopover) {
-                popoverElement.remove();
-            }
-        }, 300);
-    };
+  const removePopover = () => {
+    popoverElement.classList.remove("visible")
+    setTimeout(() => {
+      if (!isMouseOverLink && !isMouseOverPopover) {
+        popoverElement.remove()
+      }
+    }, 300)
+  }
 
-    const showPopover = () => {
-        popoverElement.classList.add("popover-visible");
-    };
+  const showPopover = () => {
+    popoverElement.classList.add("popover-visible")
+  }
 
-    linkElement.addEventListener("mouseenter", () => {
-        isMouseOverLink = true;
-        showPopover();
-    });
+  linkElement.addEventListener("mouseenter", () => {
+    isMouseOverLink = true
+    showPopover()
+  })
 
-    linkElement.addEventListener("mouseleave", () => {
-        isMouseOverLink = false;
-        removePopover();
-    });
+  linkElement.addEventListener("mouseleave", () => {
+    isMouseOverLink = false
+    removePopover()
+  })
 
-    popoverElement.addEventListener("mouseenter", () => {
-        isMouseOverPopover = true;
-    });
+  popoverElement.addEventListener("mouseenter", () => {
+    isMouseOverPopover = true
+  })
 
-    popoverElement.addEventListener("mouseleave", () => {
-        isMouseOverPopover = false;
-        removePopover();
-    });
+  popoverElement.addEventListener("mouseleave", () => {
+    isMouseOverPopover = false
+    removePopover()
+  })
 
-    popoverElement.addEventListener("click", (e) => {
-        const clickedLink = (e.target as HTMLElement).closest("a");
-        if (clickedLink && clickedLink instanceof HTMLAnchorElement) {
-            window.location.href = clickedLink.href;
-        } else {
-            window.location.href = linkElement.href;
-        }
-    });
+  popoverElement.addEventListener("click", (e) => {
+    const clickedLink = (e.target as HTMLElement).closest("a")
+    if (clickedLink && clickedLink instanceof HTMLAnchorElement) {
+      window.location.href = clickedLink.href
+    } else {
+      window.location.href = linkElement.href
+    }
+  })
 
-    // Cleanup function
-    return () => {
-        linkElement.removeEventListener("mouseenter", showPopover);
-        linkElement.removeEventListener("mouseleave", removePopover);
-        popoverElement.removeEventListener("mouseenter", () => { isMouseOverPopover = true; });
-        popoverElement.removeEventListener("mouseleave", () => {
-            isMouseOverPopover = false;
-            removePopover();
-        });
-        popoverElement.removeEventListener("click", () => { });
-    };
+  // Cleanup function
+  return () => {
+    linkElement.removeEventListener("mouseenter", showPopover)
+    linkElement.removeEventListener("mouseleave", removePopover)
+    popoverElement.removeEventListener("mouseenter", () => {
+      isMouseOverPopover = true
+    })
+    popoverElement.removeEventListener("mouseleave", () => {
+      isMouseOverPopover = false
+      removePopover()
+    })
+    popoverElement.removeEventListener("click", () => {})
+  }
 }
 
 /**
@@ -205,5 +212,5 @@ export function attachPopoverEventListeners(popoverElement: HTMLElement, linkEle
  * @returns The escaped text
  */
 export function escapeLeadingIdNumber(text: string): string {
-    return text.replace(/#(\d+)/, "#_$1");
+  return text.replace(/#(\d+)/, "#_$1")
 }
