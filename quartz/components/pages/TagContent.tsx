accfc61d5e5d3bd370fbe6f363690f84a6d00e34
Alex Turner <alex@turntrout.com>
2024-11-01 21:55:11 -0700

    Removed tags/ functionality, redirecting to the 'all tags' page

--- quartz/components/pages/TagContent.tsx
+++ quartz/components/pages/TagContent.tsx
@@ -2,15 +2,12 @@ import { Root } from "hast"
 import React from "react"
 
 import { i18n } from "../../i18n"
-import { QuartzPluginData } from "../../plugins/vfile"
 import { htmlToJsx } from "../../util/jsx"
 import { FilePath, FullSlug, getAllSegmentPrefixes, simplifySlug } from "../../util/path"
 import { PageList } from "../PageList"
 import style from "../styles/listPage.scss"
-import { formatTag } from "../TagList"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "../types"
 
-const numPages = 10
 const TagContent: QuartzComponent = (props: QuartzComponentProps) => {
   const { tree, fileData, allFiles, cfg } = props
   const slug = fileData.slug
@@ -31,81 +28,24 @@ const TagContent: QuartzComponent = (props: QuartzComponentProps) => {
       : htmlToJsx(fileData.filePath || ("" as FilePath), tree)
   const cssClasses: string[] = fileData.frontmatter?.cssclasses ?? []
   const classes = ["popover-hint", ...cssClasses].join(" ")
-  const processedTag = formatTag(tag)
-  if (tag === "/") {
-    const tags = [
-      ...new Set(
-        allFiles.flatMap((data) => data.frontmatter?.tags?.flatMap(getAllSegmentPrefixes)),
-      ),
-    ]
-    const tagItemMap: Map<string, QuartzPluginData[]> = new Map()
-    for (const tag of tags) {
-      tagItemMap.set(tag ?? "", allPagesWithTag(tag ?? ""))
-    }
-    return (
-      <div className={classes}>
-        <div>
-          {tags.map((tag) => {
-            const pages = tagItemMap.get(tag ?? "") ?? []
-            const listProps = {
-              ...props,
-              allFiles: pages,
-            }
 
-            const contentPage = allFiles.filter((file) => file.slug === `tags/${tag}`).at(0)
-
-            const root = contentPage?.htmlAst
-            const content =
-              !root || root?.children.length === 0
-                ? contentPage?.description
-                : htmlToJsx(contentPage.filePath || ("" as FilePath), root)
-
-            return (
-              <div key={tag}>
-                <h2>
-                  <a className="internal tag-link" href={`../tags/${tag}`}>
-                    {processedTag}
-                  </a>
-                </h2>
-                {content && <p>{content}</p>}
-                <div className="page-listing">
-                  <p>
-                    {pages.length > numPages && (
-                      <>
-                        {" "}
-                        <span>
-                          {i18n(cfg.locale).pages.tagContent.showingFirst({ count: numPages })}
-                        </span>
-                      </>
-                    )}
-                  </p>
-                  <PageList limit={numPages} {...listProps} />
-                </div>
-              </div>
-            )
-          })}
-        </div>
-      </div>
-    )
-  } else {
-    const pages = allPagesWithTag(tag)
-    const listProps = {
-      ...props,
-      allFiles: pages,
-    }
-
-    return (
-      <div className={classes}>
-        <article>{content}</article>
-        <div className="page-listing">
-          <p>{i18n(cfg.locale).pages.tagContent.itemsUnderTag({ count: pages.length })}</p>
-          <div>
-            <PageList {...listProps} />
-          </div>
-        </div>
-      </div>
-    )
+  const pages = allPagesWithTag(tag)
+  const listProps = {
+    ...props,
+    allFiles: pages,
   }
+
+  return (
+    <div className={classes}>
+      <article>{content}</article>
+      <div className="page-listing">
+        <p>{i18n(cfg.locale).pages.tagContent.itemsUnderTag({ count: pages.length })}</p>
+        <div>
+          <PageList {...listProps} />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 TagContent.css = style + PageList.css
