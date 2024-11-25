1696e0e1a867764503273181b5e8a24df2ed5bbd
Alex Turner <alex@turntrout.com>
2024-11-25 09:52:21 +0000

    Minor subfont tweak

--- scripts/subfont.sh
+++ scripts/subfont.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
 # scripts/subfont.sh
 
-# Only subset files that are larger than 900 bytes
-html_files=$(find public -type f -size +900c -name "*.html")
+# Only subset files that are larger than 1100 bytes
+html_files=$(find public -type f -size +1100c -name "*.html")
 
 # Count number of files
 num_files=$(echo "$html_files" | wc -w)
 echo "Subsetting fonts in $num_files files"
 
 # Run subfont on all files
-subfont $html_files --fallbacks --formats woff2 --in-place --instance --inline-css
+subfont $html_files --fallbacks --formats woff2 --in-place --instance --inline-css --no-recursive 2>/dev/null
