8b61ddabef1f60955b0cdf32945f0e550aeea442
Alex Turner <alex@turntrout.com>
2024-11-26 17:46:34 +0000

    Renamed key colors to be more descriptive

---
+++ scripts/notebooks/sed-colors.fish
@@ -0,0 +1,7 @@
+set -l color_pairs 'lightestgray:midground-faintest' 'lightergray:midground-fainter' 'lightgray:midground-faint' 'light:background' 'gray:midground' 'darkgray:midground-strong' 'dark:foreground'
+
+for pair in $color_pairs
+    set -l before_color (string split ':' $pair)[1]
+    set -l after_color (string split ':' $pair)[2]
+    sed -i".bak" "s|--$before_color|--$after_color|g" ./quartz/**/*.scss
+end
