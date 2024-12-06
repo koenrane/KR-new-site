d41692eebb708144adedd845cc8a491e3d0a3793
Alex Turner <alex@turntrout.com>
2024-12-05 21:38:39 -0800

    Disabled visual regression testing

--- scripts/subfont.sh
+++ scripts/subfont.sh
@@ -9,4 +9,4 @@ num_files=$(echo "$html_files" | wc -w)
 echo "Subsetting fonts in $num_files files"
 
 # Run subfont on all files
-subfont $html_files --fallbacks --formats woff2 --in-place --instance --inline-css --no-recursive 2>/dev/null
+subfont $html_files --formats woff2 --in-place --instance --inline-css --no-recursive 2>/dev/null
