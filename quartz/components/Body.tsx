a4ef9a72dfa138343ae4e07002678d37c119c329
Alex Turner <alex@turntrout.com>
2024-12-21 17:24:17 -0600

    Deepsource linting

--- quartz/components/Body.tsx
+++ quartz/components/Body.tsx
@@ -5,18 +5,8 @@ import clipboardScript from "./scripts/clipboard.inline"
 import clipboardStyle from "./styles/clipboard.scss"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 
-const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
-  // The children are the three main sections of the page: left, center, and right bars
-  return (
-    <>
-      {searchInterface}
-      <div id="quartz-body">{children}</div>
-    </>
-  )
-}
-
 const searchInterface = (
-  <div className="search" alt="Displays search results.">
+  <div className="search" aria-label="Displays search results.">
     <div id="search-container">
       <div id="search-space">
         <input
@@ -27,11 +17,21 @@ const searchInterface = (
           aria-label="Search"
           placeholder="Search"
         />
-        <div id="search-layout" data-preview={true}></div>
+        <div id="search-layout" data-preview></div>
       </div>
     </div>
   </div>
 )
+
+const Body: QuartzComponent = ({ children }: QuartzComponentProps) => {
+  // The children are the three main sections of the page: left, center, and right bars
+  return (
+    <>
+      {searchInterface}
+      <div id="quartz-body">{children}</div>
+    </>
+  )
+}
 
 Body.afterDOMLoaded = clipboardScript
 Body.css = clipboardStyle
