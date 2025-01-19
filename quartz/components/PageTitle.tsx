bc3a563ef53cf398b5734acd08137a8fc9efc41c
Alex Turner <alex@turntrout.com>
2025-01-18 18:17:36 -0800

    Another speculative fix to mobile pond gif issue

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,14 +9,13 @@ import { FullSlug, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 const altText = "A goose and a trout play in a pond in front of a castle."
 export const headerVideoContainer = (
-  <span id="header-video-container" spa-preserve>
+  <span id="header-video-container" spa-preserve key="header-video-container">
     <img
       src="https://assets.turntrout.com/static/pond.gif"
       id="header-gif"
       className="header-img no-select no-vsc"
       alt={altText}
       loading="eager"
-      spa-preserve
     />
   </span>
 )
