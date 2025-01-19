adcad52e5c5b017a5575bbb68ac2f79a42c87830
Alex Turner <alex@turntrout.com>
2025-01-18 18:32:36 -0800

    Trying to disable spa-preserve and see if it fixes gif issue
    Timestamped upon push.
    
    Commit range: e9710683af4415914c1cc3c7af1670497637d159
    ca57773d7fda724a332e9d622b01b4fc477221e8
    Final hash: 76dc4fe9431dc200f423e0d1fa548ff15ba62b9e12f34d7adf38dadab932f62d
    Timestamp output: 76dc4fe9431dc200f423e0d1fa548ff15ba62b9e12f34d7adf38dadab932f62d: {"error_code":0,"error_message":null,"data":{"created":true,"date_created":1737254049840,"comment":"test","hash_string":"d3493602248375ef74f6663613727d51f804b5075d1f0a81b9b251211fdfe130","timestamps":[{"seed_id":"986ea5bc-c263-40f6-9606-affe59370fc6","currency_id":0,"transaction":null,"private_key":null,"timestamp":null,"submit_status":0},{"seed_id":"49e42b0e-001a-4240-ba22-35cce897d2ca","currency_id":1,"transaction":null,"private_key":null,"timestamp":null,"submit_status":0}]}}

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable react/no-unknown-property */
 // (For the spa-preserve attribute)
 
 import React from "react"
@@ -9,7 +8,7 @@ import { FullSlug, pathToRoot } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 const altText = "A goose and a trout play in a pond in front of a castle."
 export const headerVideoContainer = (
-  <span id="header-video-container" spa-preserve key="header-video-container">
+  <span id="header-video-container" key="header-video-container">
     <img
       src="https://assets.turntrout.com/static/pond.gif"
       id="header-gif"
